dc365d33a4109d9627c2b6380c18bdc62a7c9f6e
shamlik <tshamlikt@gmail.com>
2017-03-02 11:56:02 +0530

    Adds password field to model

--- model.py
+++ model.py
@@ -1,24 +1,30 @@
-import datetime
+
 
 from playhouse.sqlcipher_ext import *
 
-db = SqlCipherDatabase('user_data.db', passphrase='testtest')
+database='user_data.db'
 
+
+db = SqlCipherDatabase(database, passphrase='testtest')
 
 class Category(Model):
-    category_id = IntegerField()
+    category_id = IntegerField(unique=True)
     category_name = CharField()
 
     class Meta:
         database = db
 
 class Customer(Model):
-    customer_id = IntegerField()
+    customer_id = IntegerField(unique=True)
     category_id = ForeignKeyField(Category, related_name='category')
     age = IntegerField()
     phone = CharField()
     address = TextField()
     customer_name = CharField()
-
+    password=CharField()
     class Meta:
         database = db
+
+if not db.get_tables():
+    db.create_tables([Category, Customer])
+    
